7931ece0e94502dc7204f11b2c152d60c182658a
yiyi <yiyi119120@gmail.com>
2017-12-12 14:16:54 +0800

    append change

---
+++ src/com/yiyi/collections/Hashmap.java
@@ -0,0 +1,16 @@
+package com.yiyi.collections;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * Created by zgx on 2017-12-11.
+ */
+public class Hashmap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.peek();
+        linkedList.isEmpty();
+    }
+}
